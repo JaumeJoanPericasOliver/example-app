c91ce33974c0c1113c9ce53c21a23c8aadc67c44
JaumeJoanPericasOliver <134274926+JaumeJoanPericasOliver@users.noreply.github.com>
2024-09-16 18:57:16 +0200

    Commit 2 del 16-09-2024

--- resources/views/post/edit.blade.php
+++ resources/views/post/edit.blade.php
@@ -12,22 +12,25 @@
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900 dark:text-gray-100">
                     
-                    <form action="{{ route('post.update', $post->id) }}" method="post">
+                    <form action="{{ route('post.update', $post->id) }}" method="post" class="mt-6 space-y-6" >
                         @csrf
                         @method('PUT')
-                        <label for="title">Títol</label>
-                        <input type="text" style="@error('title') border-color: RED;  @enderror"  value="{{ $post->title }}" name="title" />
-                        @error('title')
-                            <div>{{ $message }}</div>
-                        @enderror
-                        <label for="url_clean">Url neta</label>
-                        <input type="text" name="url_clean" value="{{ $post->url_clean }}" />
-                        @error('title')
-                            <div>{{ $message }}</div>
-                        @enderror
-                        <label for="content">Contingut</label>
-                        <textarea name="content" col="3" >{{ $post->content }}</textarea>
-                        <input type="submit" value="Crear" >
+                        <div>
+                            <x-input-label for="title" :value="__('Títol')" />
+                            <x-text-input id="title" name="title" type="text" value="{{ $post->title }}" class="mt-1 block w-full" />
+                            <x-input-error :messages="$errors->get('title')" class="mt-2" />
+                        </div>
+                        <div>
+                            <x-input-label for="url_clean" :value="__('Url neta')" />
+                            <x-text-input id="url_clean" name="url_clean" value="{{ $post->url_clean }}" type="text" class="mt-1 block w-full" />
+                            <x-input-error :messages="$errors->get('url_clean')" class="mt-2" />
+                        </div>
+                        <div>
+                            <x-input-label for="content" :value="__('Contingut')" />
+                            <textarea name="content" col="3" class="mt-1 block w-full" >{{ $post->content }}</textarea>
+                            <x-input-error :messages="$errors->get('content')" class="mt-2" />
+                        </div>
+                        <input type="submit" value="Modificar" class="inline-flex items-center px-4 py-2 bg-gray-800 dark:bg-gray-200 border border-transparent rounded-md font-semibold text-xs text-white dark:text-gray-800 uppercase tracking-widest hover:bg-gray-700 dark:hover:bg-white focus:bg-gray-700 dark:focus:bg-white active:bg-gray-900 dark:active:bg-gray-300 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 dark:focus:ring-offset-gray-800 transition ease-in-out duration-150" >
                     </form>
                 </div>
             </div>
